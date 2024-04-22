24833de50d977bb84a2902ed5b16c6acafed96b9
avarxx <valeev.anvar@phystech.edu>
2024-04-22 14:17:08 +0300

    fix

--- Sort/Testing/testing.h
+++ Sort/Testing/testing.h
@@ -11,37 +11,37 @@
 
 #include "C:\algorithms\Lab2024\Sort\QuadraticSort\quadraticSort.h"
 
-#include "C:\algorithms\Lab2024\Sort\QuadraticSort\babbleSort.cpp"
-#include "C:\algorithms\Lab2024\Sort\QuadraticSort\insertionSort.cpp"
-#include "C:\algorithms\Lab2024\Sort\QuadraticSort\selectionSort.cpp"
-#include "C:\algorithms\Lab2024\Sort\QuadraticSort\shell.cpp"
+#include "C:\algorithms\Lab2024\Sort\QuadraticSort\bubbleSort.c"
+#include "C:\algorithms\Lab2024\Sort\QuadraticSort\insertionSort.c"
+#include "C:\algorithms\Lab2024\Sort\QuadraticSort\selectionSort.c"
+#include "C:\algorithms\Lab2024\Sort\QuadraticSort\shell.c"
 
 #include "C:\algorithms\Lab2024\Sort\MergeSort\merge.h"
 
-#include "C:\algorithms\Lab2024\Sort\MergeSort\mergeIterative.cpp"
-#include "C:\algorithms\Lab2024\Sort\MergeSort\mergeRecursion.cpp"
+#include "C:\algorithms\Lab2024\Sort\MergeSort\mergeIterative.c"
+#include "C:\algorithms\Lab2024\Sort\MergeSort\mergeRecursion.c"
 
 #include "C:\algorithms\Lab2024\Sort\Qsort4\qsort.h"
 
-#include "C:\algorithms\Lab2024\Sort\Qsort4\qsortFlag.cpp"
-#include "C:\algorithms\Lab2024\Sort\Qsort4\qsortHoara.cpp"
-#include "C:\algorithms\Lab2024\Sort\Qsort4\qsortLamuto.cpp"
+#include "C:\algorithms\Lab2024\Sort\Qsort4\qsortFlag.c"
+#include "C:\algorithms\Lab2024\Sort\Qsort4\qsortHoara.c"
+#include "C:\algorithms\Lab2024\Sort\Qsort4\qsortLamuto.c"
 
 #include "C:\algorithms\Lab2024\Sort\Qsort5\qsort5.h"
 
-#include "C:\algorithms\Lab2024\Sort\Qsort5\qsortElem.cpp"
-#include "C:\algorithms\Lab2024\Sort\Qsort5\qsortMedian.cpp"
-#include "C:\algorithms\Lab2024\Sort\Qsort5\qsortMedianRand.cpp"
-#include "C:\algorithms\Lab2024\Sort\Qsort5\qsortRand.cpp"
+#include "C:\algorithms\Lab2024\Sort\Qsort5\qsortElem.c"
+#include "C:\algorithms\Lab2024\Sort\Qsort5\qsortMedian.c"
+#include "C:\algorithms\Lab2024\Sort\Qsort5\qsortMedianRand.c"
+#include "C:\algorithms\Lab2024\Sort\Qsort5\qsortRand.c"
 
-#include "C:\algorithms\Lab2024\Sort\PyramidSort\heapSort.cpp"
+#include "C:\algorithms\Lab2024\Sort\PyramidSort\heapSort.c"
 
-#include "C:\algorithms\Lab2024\Sort\SortingBytes\lsd.cpp"
-#include "C:\algorithms\Lab2024\Sort\SortingBytes\msd.cpp"
+#include "C:\algorithms\Lab2024\Sort\SortingBytes\lsd.c"
+#include "C:\algorithms\Lab2024\Sort\SortingBytes\msd.c"
 
 #define lenStr 40
 
-void testing        (char* dir, void (*sort)(int* arr, size_t n), const char* results, size_t from, size_t to, size_t step);
+void testing        (const char* dir, void (*sort)(int* arr, size_t n), const char* results, size_t from, size_t to, size_t step, int numberOftest);
 
 void testQuadratic  (void);
 
